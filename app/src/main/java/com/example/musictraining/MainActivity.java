be48cc1a4cf666944f05999d5415075d38da0906
Amanda <anakanishi@uchicago.edu>
2019-02-14 18:14:26 -0600

    We can now write to files stored under: My Files/Android/data/com.example.musictraining

--- app/src/main/java/com/example/musictraining/MainActivity.java
+++ app/src/main/java/com/example/musictraining/MainActivity.java
@@ -5,17 +5,26 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import android.app.Activity;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1=null;
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private Sensor gyroSensor;
     private Sensor linAccelSensor;
     private Sensor accelSensor;
+    public static final String LOG_TAG = "DIRECTORY-STATUS";
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         tv1 = (TextView) findViewById(R.id.textView2);
-        tv1.setVisibility(View.GONE);
+        //tv1.setVisibility(View.GONE);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -74,8 +84,65 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //@Override
-    // TODO: Figure out if on click or like on other thing =/
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getPublicStorageDir(String fileName) {
+        // Get the directory for the public files directory.
+        //File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "new.txt");
+        File file = new File(this.getExternalFilesDir(null), fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                tv1.setVisibility(View.VISIBLE);
+                tv1.append("did not work\n");
+            }
+        }
+        return file;
+    }
+
+    /** Called when the user touches the button */
+    public void sendToFile(View view)
+    {
+        // Do something in response to button click
+
+        tv1 = findViewById(R.id.textView2);
+        tv1.setVisibility(View.VISIBLE);
+        tv1.append("hello\n");
+
+        // File stored in MyFiles > com.example.musictraining
+        String fileName = "test_file1.txt";
+
+        File file = getPublicStorageDir(fileName);
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(file, true /*append*/));
+            writer.write("This is a test file.");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            tv1.append("bufferwriter");
+
+        }
+
+        tv1.append("made it");
+
+        // Note: based on this test, can access external storage on my phone
+//        boolean isWritable = isExternalStorageWritable();
+//        if(isWritable == true)
+//            tv1.append("can write and read!\n");
+
+
+
+    }
 
 
 }
